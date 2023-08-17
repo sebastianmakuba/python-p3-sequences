2854b07ad8e0666f0410417c9835d2e07416c602
+ <+>
2023-08-17 20:23:55 +0300

    completed assignement

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+  fib_sequence = []
+  if length >= 1:
+    fib_sequence.append(0)
+  if length >=2:
+    fib_sequence.append(1)
+
+  for x in range(2, length):
+    next_fibonacci_number = fib_sequence[x-1] + fib_sequence[x-2]
+    fib_sequence.append(next_fibonacci_number)
+  print(fib_sequence)
+
+print_fibonacci(9)
